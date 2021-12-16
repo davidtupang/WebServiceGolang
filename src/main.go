d6d8bb7040198a6976d14937a4bb36fb51bae991
davidtupang <davidprdmnsimatupang@gmail.com>
2021-12-16 12:28:33 +0700

    Commit Get Param Query

--- src/main.go
+++ src/main.go
@@ -9,17 +9,40 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "David Pardamean Simatupang",
-			"bio":  "Backend Developer and Software Engineer",
-		})
+	router.GET("/", rootHandler)
+	router.GET("/hello", helloHandler)
+	router.GET("/produk/:id/:judul", detailProduk)
+	router.GET("/query/", detailQuery)
+	router.Run()
+
+}
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "David Pardamean Simatupang",
+		"bio":  "Backend Developer and Software Engineer",
 	})
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":     "Web Service API GOLANG - David P Simatupang",
-			"deskripsi": "Terimakasih",
-		})
+}
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":     "Web Service API GOLANG - David P Simatupang",
+		"deskripsi": "Terimakasih",
 	})
-	router.Run(":8888")
+}
+
+func detailProduk(c *gin.Context) {
+	id := c.Param("id")
+	judul := c.Param("judul")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"judul": judul,
+	})
+}
+
+func detailQuery(c *gin.Context) {
+	id := c.Query("id")
+	title := c.Query("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
 }
